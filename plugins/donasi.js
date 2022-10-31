fe2a8dad5bf958c100ad0e288b23d3bd915990e7
RaffGremory <takinadesuwa@gmail.com>
2022-11-01 01:24:43 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -9,7 +9,7 @@ const catalog = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
 "product": {
 "productImage": messa.imageMessage, 
 "productId": "5489299044451325",
-"title": `Jasa Sewa Yushino Md`,
+"title": `Jasa Sewa Yor Bot`,
 "description": `HALO BANG`,
 "currencyCode": "IDR",
 "bodyText": wm,
@@ -19,9 +19,9 @@ const catalog = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
 "firstImageId": 1,
 "salePriceAmount1000": "10000000",
 "retailerId": wm,
-"url": "wa.me/6281361281833"
+"url": "wa.me/6281332093398"
 },
-"businessOwnerJid": "6281361281833@s.whatsapp.net",
+"businessOwnerJid": "6281332093398@s.whatsapp.net",
 }
 }), { userJid: m.chat, quoted: ftroli })    
 
